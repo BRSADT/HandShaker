fb08cfd8ddfc6cca39033dda6e85c4fc8d565094
Daniel Castañeda Mejia <gaming.sub04@gmail.com>
2021-03-17 09:05:27 -0600

    Commented Stack Navigator

--- frontend/src/Movil/App.js
+++ frontend/src/Movil/App.js
@@ -2,9 +2,9 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import ClientProfile from './src/Movil/Vistas/ClientProfile';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/Stack';
+//import { createStackNavigator } from '@react-navigation/Stack';
 
-const Stack = createStackNavigator();
+//const Stack = createStackNavigator();
 
 export default class App extends Component {
   render() {
