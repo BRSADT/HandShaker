dfb8878903a131ee6d0e55f1d07863dabfb40248
BRSADT <brendasamant@hotmail.com>
2021-05-27 18:48:45 -0500

    model Address update

--- src/Interfaces/IAddress.ts
+++ src/Interfaces/IAddress.ts
@@ -2,6 +2,9 @@ import { Document} from 'mongoose';
 export interface IAddress extends Document{
     Address:string
     Number:string
+    BAddress1:string
+    BAddress2:string
+    neighborhood:string
     Reference:string
     LinkMaps:string
     Id:String
